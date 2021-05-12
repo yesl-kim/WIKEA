db5344f74471d413b2066fc4beda356fd2008aca
Kim Yeseul <96linda@hanmail.net>
2021-05-13 00:48:49 +0900

    Add : children prop 추가, 닫힘 버튼 기능 추가

--- src/components/sideModal/sideModal.js
+++ src/components/sideModal/sideModal.js
@@ -3,17 +3,31 @@ import React, { Component } from 'react';
 import './SideModal.scss';
 
 class SideModal extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isOn: true,
+    };
+  }
+
+  handleClick = () => {
+    console.log('clicked');
+    this.setState({ isOn: false });
+  };
+
   render() {
-    const { title, direction } = this.props;
+    const { title, direction, children } = this.props;
+    const { isOn } = this.state;
     return (
-      <div className="sideModal">
+      <div className={isOn ? 'sideModal on' : 'sideModal'}>
         <div className="dimmer" />
         <div className={`nav_like ${direction}`}>
           <h1 className="visually-hidden">{title}</h1>
-          <button type="button" className="close">
+          <button type="button" className="close" onClick={this.handleClick}>
             <i className="ic-close" />
           </button>
         </div>
+        {children}
       </div>
     );
   }
