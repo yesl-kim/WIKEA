4de1a7764d96a96da0b84a1b86cfdc14bc6aa3d8
SUYEON <gsy4568@gmail.com>
2021-05-12 19:09:54 +0900

    Add : Product component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,6 +3,46 @@ import './Footer.scss';
 
 class Footer extends React.Component {
   render() {
+    const linkLines = {
+      question: [
+        { name: '고객지원', url: '##' },
+        { name: '자주 묻는 질문', url: '##' },
+        { name: '문의하기', url: '##' },
+        { name: '배송조회', url: '##' },
+        { name: '교환환불', url: '품질보증' },
+        { name: '제품리콜', url: '##' },
+        { name: '제품 구매 가이드', url: '##' },
+        { name: '피드백', url: '##' },
+      ],
+      shopping: [
+        { name: '쇼핑하기', url: '##' },
+        { name: '전화 주문', url: '##' },
+        { name: 'WIKEA for Business', url: '##' },
+        { name: '셀프 플래닝', url: '##' },
+        { name: 'WIKEA 모바일 앱', url: '품질보증' },
+        { name: 'WIKEA 카탈로그', url: '##' },
+        { name: '제품 사용 팁/가이드', url: '##' },
+        { name: '결제 옵션', url: '##' },
+        { name: '기프트 카드', url: '##' },
+      ],
+      servise: [
+        { name: 'WIKEA 서비스', url: '##' },
+        { name: '배송 서비스', url: '##' },
+        { name: '조립 서비스', url: '##' },
+        { name: '주방 서비스', url: '##' },
+        { name: '욕실 서비스', url: '품질보증' },
+        { name: '홈퍼니싱 플래닝 서비스', url: '##' },
+        { name: '인테리어 디자인 서비스', url: '##' },
+        { name: '바이백 서비스', url: '##' },
+      ],
+      story: [
+        { name: 'Wecode 소개', url: '##' },
+        { name: '데모크리틱 디자인', url: '##' },
+        { name: '지속가능한 생활', url: '##' },
+        { name: '뉴스', url: '##' },
+        { name: '채용 정보', url: '품질보증' },
+      ],
+    };
     return (
       <footer className="footer">
         <div className="row footerLinks">
@@ -17,46 +57,35 @@ class Footer extends React.Component {
           </div>
           <div className="links col-lg-2">
             <h4>고객문의</h4>
-            <div>고객지원</div>
-            <div>자주 묻는 질문</div>
-            <div>문의하기</div>
-            <div>배송조회</div>
-            <div>교환환불</div>
-            <div>품질보증</div>
-            <div>제품리콜</div>
-            <div>제품 구매 가이드</div>
-            <div>피드백</div>
+            {linkLines.question.map(el => (
+              <div>
+                <a href={el.url}>{el.name}</a>
+              </div>
+            ))}
           </div>
           <div className="links col-lg-2">
             <h4>쇼핑하기</h4>
-            <div>쇼핑하기</div>
-            <div>전화 주문</div>
-            <div>WIKEA for Business</div>
-            <div>셀프 플래닝</div>
-            <div>WIKEA 모바일 앱</div>
-            <div>WIKEA 카탈로그</div>
-            <div>제품 사용 팁/가이드</div>
-            <div>결제 옵션</div>
-            <div>기프트 카드</div>
+            {linkLines.shopping.map(el => (
+              <div>
+                <a href={el.url}>{el.name}</a>
+              </div>
+            ))}
           </div>
           <div className="links col-lg-2">
             <h4>서비스</h4>
-            <div>WIKEA 서비스</div>
-            <div>배송 서비스</div>
-            <div>조립 서비스</div>
-            <div>주방 서비스</div>
-            <div>욕실 서비스</div>
-            <div>홈퍼니싱 플래닝 서비스</div>
-            <div>인테리어 디자인 서비스</div>
-            <div>바이백 서비스</div>
+            {linkLines.servise.map(el => (
+              <div>
+                <a href={el.url}>{el.name}</a>
+              </div>
+            ))}
           </div>
           <div className="links col-lg-2">
             <h4>WIKEA 이야기</h4>
-            <div>Wecode 소개</div>
-            <div>데모크래틱 디자인</div>
-            <div>지속가능한 생활</div>
-            <div>뉴스</div>
-            <div>채용정보</div>
+            {linkLines.story.map(el => (
+              <div>
+                <a href={el.url}>{el.name}</a>
+              </div>
+            ))}
           </div>
         </div>
         <div></div>
--- src/pages/Main/Lists/Products/Product/Product.js
+++ src/pages/Main/Lists/Products/Product/Product.js
@@ -2,18 +2,24 @@ import React from 'react';
 
 class Product extends React.Component {
   render() {
+    const { item } = this.props;
+
     return (
       <div className="product">
+        <input type="checkbox" />
+        <button>
+          <i className="ic-heart" />
+        </button>
         <img
           src={
             'https://www.ikea.com/kr/ko/images/products/arstid-…ble-lamp-brass-white__0880725_pe617347_s5.jpg?f=m'
           }
         />
         <div className="productsExplanation">
-          <h2>
+          <h2 className="productName">
             <span>멋있는 램프</span>
           </h2>
-          <div className="productSize">탁상스탠드, 29 cm</div>
+          <div className="productSize">탁상스탠드</div>
           <div className="productPrice">
             <span>₩</span>5,000
           </div>
