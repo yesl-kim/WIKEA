92274fb5eaa9f2160ad6e0a2dbc7d5691b9e3934
sunkyungnoh <cellin1637@gmail.com>
2021-05-13 00:16:51 +0900

    모달 컴포넌트로 나눔

--- src/pages/Main/Details/Details.js
+++ src/pages/Main/Details/Details.js
@@ -3,6 +3,7 @@ import Message from '../../../components/Header/Message';
 import Header from '../../../components/Header/Header';
 import Main from './Main/Main.js';
 import './Details.scss';
+import ImgModal from './Modal/ImgModal';
 
 class Details extends React.Component {
   constructor() {
@@ -27,17 +28,7 @@ class Details extends React.Component {
         <Message />
         <Header />
         <Main modalOn={modalOn} handleModal={this.handleModal} />
-        {modalOn && (
-          <div className="modal">
-            <div className="modal_back">
-              <img
-                alt="상세 이미지"
-                src="https://www.ikea.com/kr/ko/images/products/arstid-…ble-lamp-brass-white__0880725_pe617347_s5.jpg?f=m"
-              />
-              <button onClick={this.handleModal} className="modal_btn" />
-            </div>
-          </div>
-        )}
+        {modalOn && <ImgModal handleModal={this.handleModal} />}
       </>
     );
   }
