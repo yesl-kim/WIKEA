4406777e3178d247a628d6cec0b90777e7e6d173
SUYEON <gsy4568@gmail.com>
2021-05-12 20:52:54 +0900

    수정중---

--- src/pages/Main/Lists/Products/Product/Product.js
+++ src/pages/Main/Lists/Products/Product/Product.js
@@ -8,28 +8,32 @@ class Product extends React.Component {
       <div className="product">
         {/* children으로 구멍뚫어 놓기(input checkbox) */}
         <input type="checkbox" />
-        <button>
+        <button className="product_favorite">
           <i className="ic-heart" />
         </button>
         <img
+          alt="lamp"
           src={
             'https://www.ikea.com/kr/ko/images/products/arstid-…ble-lamp-brass-white__0880725_pe617347_s5.jpg?f=m'
           }
         />
-        <div className="productsExplanation">
-          <h2 className="productName">
+        <div className="products_explanation">
+          <h2 className="produc_name">
             <span>멋있는 램프</span>
           </h2>
-          <div className="productSize">탁상스탠드</div>
-          <div className="productPrice">
+          <div className="product_size">탁상스탠드</div>
+          <div className="product_price">
             <span>₩</span>5,000
           </div>
-          <div>
+          <div className="product_rating">
             <i className="ic-star" />
             <i className="ic-star" />
             <i className="ic-star" />
             <i className="ic-star" />
           </div>
+          <button className="product_shopping">
+            <i className="ic-cart" />
+          </button>
         </div>
       </div>
     );
