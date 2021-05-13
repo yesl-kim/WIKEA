3ffd5747ac3f373149b9451ed6832fc83ec760ff
sunkyungnoh <cellin1637@gmail.com>
2021-05-13 17:06:54 +0900

    Nav 안으로 Message 이동

--- src/pages/Main/Details/Details.js
+++ src/pages/Main/Details/Details.js
@@ -1,15 +1,11 @@
 import React from 'react';
-import Message from '../../../components/Nav/Message';
 import Nav from '../../../components/Nav/Nav';
-// import Main from './Main/Main.js';
 
 class Details extends React.Component {
   render() {
     return (
       <>
-        <Message />
         <Nav />
-        {/* <Main /> */}
       </>
     );
   }
