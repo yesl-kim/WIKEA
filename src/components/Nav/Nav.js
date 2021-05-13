d5c220de93b918b802811a6d80d0916d68024584
sunkyungnoh <cellin1637@gmail.com>
2021-05-13 13:39:36 +0900

    전체 레이아웃 위치 수정: 사이드바 버튼 위치 git push origin feature/Header

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,60 +4,56 @@ import './Nav.scss';
 class Nav extends React.Component {
   render() {
     return (
-      <div className="grid-container">
-        <div className="row">
-          <nav>
-            <h1 className="visually-hidden">메뉴바</h1>
-            <img alt="logo" src="/images/Detail/logo.png" />
-            <div className="lg-only options">
-              <a href="/">
-                <span>모든제품</span>
-              </a>
-              <a href="/">
-                <span>디지털 쇼룸</span>
-              </a>
-            </div>
-            <ul className="nav-menu-group">
-              <li>
-                <a href="/">
-                  <i className="ic-cart1 lg-only" />
-                </a>
-              </li>
-              <li>
-                <a href="/">
-                  <i className="ic-user" />
-                </a>
-              </li>
-              <li>
-                <a href="/">
-                  <i className="ic-heart" />
-                </a>
-              </li>
-              <li>
-                <a href="/">
-                  <i className="ic-cart" />
-                </a>
-              </li>
-              <li>
-                <button className="col-lg-1">
-                  <i className="ic-menu" />
-                </button>
-              </li>
-            </ul>
-            <div className="searchBox">
-              <i className="ic-search" />
-              <input
-                aria-label="제품 및 아이디어, 신제품 검색"
-                placeholder="검색어 입력"
-                type="search"
-              />
-              <button>
-                <i className="ic-store" />
-              </button>
-            </div>
-          </nav>
+      <nav>
+        <h1 className="visually-hidden">메뉴바</h1>
+        <img alt="logo" src="/images/Detail/logo.png" />
+        <div className="lg-only options">
+          <a href="/">
+            <span>모든제품</span>
+          </a>
+          <a href="/">
+            <span>디지털 쇼룸</span>
+          </a>
         </div>
-      </div>
+        <ul className="nav-menu-group">
+          <li>
+            <a href="/">
+              <i className="ic-cart1 lg-only" />
+            </a>
+          </li>
+          <li>
+            <a href="/">
+              <i className="ic-user" />
+            </a>
+          </li>
+          <li>
+            <a href="/">
+              <i className="ic-heart" />
+            </a>
+          </li>
+          <li>
+            <a href="/">
+              <i className="ic-cart" />
+            </a>
+          </li>
+          <li>
+            <button className="col-lg-1">
+              <i className="ic-menu" />
+            </button>
+          </li>
+        </ul>
+        <div className="searchBox">
+          <i className="ic-search" />
+          <input
+            aria-label="제품 및 아이디어, 신제품 검색"
+            placeholder="검색어 입력"
+            type="search"
+          />
+          <button>
+            <i className="ic-store" />
+          </button>
+        </div>
+      </nav>
     );
   }
 }
