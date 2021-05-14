56b299d991f9fbee7957f268a968013976ef9644
SUYEON <gsy4568@gmail.com>
2021-05-14 23:41:37 +0900

    Add : lists page layout revise

--- src/pages/Main/Lists/ItemExplanation/ItemExplanation.js
+++ src/pages/Main/Lists/ItemExplanation/ItemExplanation.js
@@ -4,7 +4,7 @@ import './ItemExplanation.scss';
 class ItemExplanation extends React.Component {
   render() {
     return (
-      <div className="item_explanation">
+      <section className="item_explanation">
         <h1>탁상스탠드</h1>
         <p>
           탁상스탠드는 실용적이면서도 방안에 개성적인 느낌을 더해줄 수 있어용.
@@ -12,7 +12,7 @@ class ItemExplanation extends React.Component {
           조명으로 활용하면 더 편안하게 잠들 수 있죠. 디자인이 다양한 만큼
           스타일도 다양해요. 머리 위의 조명을 껐을 때는 탁상스탠드를 켜보세요.
         </p>
-      </div>
+      </section>
     );
   }
 }
--- src/pages/Main/Lists/ListBtn/Sortbox/Sortbox.js
+++ src/pages/Main/Lists/ListBtn/Sortbox/Sortbox.js
@@ -4,63 +4,28 @@ import './Sortbox.scss';
 class Sortbox extends React.Component {
   render() {
     const { className } = this.props;
+    const btnTitle = [
+      '베스트 매치',
+      '낮은 가격 순',
+      '높은 가격 순',
+      '최신',
+      '고객 평가',
+      '이름',
+      '가장 인기있는',
+      '폭',
+      '신장',
+    ];
 
     return (
       <div className={`Sortbox ${className}`}>
-        <label className="radio_btn">
-          <span>베스트 매치</span>
-          <div className="radio">
-            <input type="radio" id="radio" name="radioname" />
-          </div>
-        </label>
-        <label className="radio_btn">
-          <span>낮은 가격 순</span>
-          <div className="radio">
-            <input type="radio" id="radio" name="radioname" />
-          </div>
-        </label>
-        <label className="radio_btn">
-          <span>높은 가격 순</span>
-          <div className="radio">
-            <input type="radio" id="radio" name="radioname" />
-          </div>
-        </label>
-        <label className="radio_btn">
-          <span>최신</span>
-          <div className="radio">
-            <input type="radio" id="radio" name="radioname" />
-          </div>
-        </label>
-        <label className="radio_btn">
-          <span>고객 평가</span>
-          <div className="radio">
-            <input type="radio" id="radio" name="radioname" />
-          </div>
-        </label>
-        <label className="radio_btn">
-          <span>이름</span>
-          <div className="radio">
-            <input type="radio" id="radio" name="radioname" />
-          </div>
-        </label>
-        <label className="radio_btn">
-          <span>가장 인기있는</span>
-          <div className="radio">
-            <input type="radio" id="radio" name="radioname" />
-          </div>
-        </label>
-        <label className="radio_btn">
-          <span>폭</span>
-          <div className="radio">
-            <input type="radio" id="radio" name="radioname" />
-          </div>
-        </label>
-        <label className="radio_btn">
-          <span>신장</span>
-          <div className="radio">
-            <input type="radio" id="radio" name="radioname" />
-          </div>
-        </label>
+        {btnTitle.map(title => (
+          <label className="radio_btn">
+            <span>{title}</span>
+            <div className="radio">
+              <input type="radio" id="radio" name="radioname" />
+            </div>
+          </label>
+        ))}
       </div>
     );
   }
