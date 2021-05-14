d12cb4ed5e1a1ddb2dbc1fd0727452ba9605aec4
SUYEON <gsy4568@gmail.com>
2021-05-14 17:59:57 +0900

    product component version3

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,34 +2,69 @@ import React from 'react';
 import './Product.scss';
 
 class Product extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      favoriteBtn: false,
+    };
+  }
+
+  handleFavoriteBtn = () => {
+    const { favoriteBtn } = this.state;
+
+    this.setState({
+      favoriteBtn: !favoriteBtn,
+    });
+  };
+
   render() {
-    const { item } = this.props;
+    const {
+      name,
+      category,
+      price,
+      isFavorite,
+      rating,
+      img,
+      classGrid,
+      children,
+    } = this.props;
+
+    const { favoriteBtn } = this.state;
+
+    const totalRating = () => {
+      const ratingIcons = [];
+      for (let i = 0; i <= this.props.rating - 1; i++) {
+        ratingIcons.push(<i key={i} className="ic-star" />);
+      }
+      if (rating % 1 > 0) {
+        ratingIcons.push(<i className="ic-cart" />);
+      }
+
+      return ratingIcons;
+    };
 
     return (
-      <div className="product">
-        {this.props.children}
-        <button className="product_favorite">
-          <i className="ic-heart" />
+      <div className={`product ${classGrid}`}>
+        {children}
+        <button className="product_favorite" onClick={this.handleFavoriteBtn}>
+          <i className={`ic-heart ${favoriteBtn}`} />
         </button>
         <img
-          alt="lamp"
-          src="https://www.ikea.com/kr/ko/images/products/arstid-…ble-lamp-brass-white__0880725_pe617347_s5.jpg?f=m"
+          alt={category}
+          src={img[0]}
+          onMouseOver={e => img[1] && (e.currentTarget.src = img[1])}
+          onMouseOut={e => (e.currentTarget.src = img[0])}
         />
         <div className="products_explanation">
-          <div className="product_new">NEW</div>
-          <h2 className="product_name">
-            <span>멋있는 램프</span>
+          <h2 className="produc_name">
+            <span>{name}</span>
           </h2>
-          <div className="product_size">탁상스탠드</div>
+          <div className="product_size">{category}</div>
           <div className="product_price">
-            <span>₩</span>5,000
+            <span>₩</span>
+            {price}
           </div>
-          <div className="product_rating">
-            <i className="ic-star" />
-            <i className="ic-star" />
-            <i className="ic-star" />
-            <i className="ic-star" />
-          </div>
+          <div className="product_rating">{totalRating()}</div>
           <button className="product_shopping">
             <i className="ic-cart" />
           </button>
---
+++ src/components/Product/README.md
@@ -0,0 +1,28 @@
+# Product Component
+
+### state
+
+- favoriteBTn : 위시리스트 버튼 색을 변경하기 위한 state입니다.
+
+---
+
+### function
+
+- handleFavoriteBtn : 위시리스트 버튼 state를 변경하기 위한 함수입니다
+- totalRating : 별점 점수에 따라 아이콘을 넣어주는 함수입니다.
+  (반쪽짜리 별점 아이콘이 없어 일단 ic-cart로 대체했습니다!)
+
+---
+
+### props
+
+- name : 제품 명
+- category : 제품 카테고리
+- price : 제품 가격
+- isFavorite : 제품의 위시리스트 상태
+- rating : 제품 별점
+- img : 제품 url
+
+- classGrid : product component에 그리드 속성을 부여하기 위한 className입니다
+
+- children : <input type="checkbox">를 담기위한 children입니다
