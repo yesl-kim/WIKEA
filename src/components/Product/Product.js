54b4f37c7f725b885b2cc3fb0e70b3a342716f99
SUYEON <gsy4568@gmail.com>
2021-05-17 18:19:05 +0900

    test

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -17,24 +17,25 @@ class Product extends React.Component {
     });
   };
 
+  totalRating = () => {
+    const { rating } = this.props;
+    const ratingIcons = [];
+
+    for (let i = 0; i <= this.props.rating - 1; i++) {
+      ratingIcons.push(<i className="ic-star" />);
+    }
+    if (rating % 1 > 0) {
+      ratingIcons.push(<i className="ic-cart" />);
+    }
+
+    return ratingIcons;
+  };
+  // newArray(rating).fill활용하여 수정하기
+
   render() {
-    const { name, category, price, isNew, rating, img, classGrid, children } =
+    const { koName, enName, category, price, isNew, img, classGrid, children } =
       this.props;
     const { favoriteBtn } = this.state;
-
-    const totalRating = () => {
-      const ratingIcons = [];
-      for (let i = 0; i <= this.props.rating - 1; i++) {
-        ratingIcons.push(<i className="ic-star" />);
-      }
-      if (rating % 1 > 0) {
-        ratingIcons.push(<i className="ic-cart" />);
-      }
-
-      // newArray(rating).fill활용하여 수정하기
-      return ratingIcons;
-    };
-    // 함수 밖으로 빼주기
 
     return (
       <div className={`product ${classGrid}`}>
@@ -49,14 +50,14 @@ class Product extends React.Component {
         <div className="products_explanation">
           <div className="product_new">{isNew ? 'New' : ''}</div>
           <h2 className="product_name">
-            <span>{name}</span>
+            <span>{enName + koName}</span>
           </h2>
           <div className="product_size">{category}</div>
           <div className="product_price">
             <span>₩</span>
             {price}
           </div>
-          <div className="product_rating">{totalRating()}</div>
+          <div className="product_rating">{this.totalRating()}</div>
           <button className="product_shopping">
             <i className="ic-cart" />
           </button>
