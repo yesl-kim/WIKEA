17f44426b24abab546bf8e1e1c78aec5546a1a8d
Kim Yeseul <96linda@hanmail.net>
2021-05-17 13:24:04 +0900

    Modify : main.js 복구, 머지되지 않은 컴포넌트 제거

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import MainNavModal from '../../components/MainNavModal/MainNavModal';
 import './Main.scss';
 
 class Main extends React.Component {
   render() {
-    return (
-      <>
-        <MainNavModal />
-      </>
-    );
+    return <></>;
   }
 }
 
