8f4adbf5b910dd7235aaa9a95e1a186343ad879b
sunkyungnoh <cellin1637@gmail.com>
2021-05-18 11:27:11 +0900

    detail modal 구현

--- src/pages/Main/Details/Modal/DetailsModal/DetailsModal.js
+++ src/pages/Main/Details/Modal/DetailsModal/DetailsModal.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Detail from './Detail';
 // import Details from '../Details';
 import './DetailsModal.scss';
 
@@ -21,13 +22,9 @@ class DetailsModal extends Component {
     const { details } = this.props;
     const { height } = this.state;
     return (
-      <dl className="details_modal">
-        <dt className="content_title">
-          <button>
-            <h1>제품 설명</h1>
-          </button>
-        </dt>
-        <dd className="content">
+      <div className="product_details">
+        <h2 className="product_details_title">제품 설명</h2>
+        <div className="product_details_container">
           {details[0].content
             .split('.')
             .slice(0, 2)
@@ -45,58 +42,32 @@ class DetailsModal extends Component {
                 return <span key={idx}>{text}</span>;
               })}
           </div>
-        </dd>
-        <hr />
-        <dt className="material_title">
-          <button>
-            <h2>소재 관리</h2>
-            <i className="ic-chevron" />
-          </button>
-        </dt>
-        <dd className="material_desc">
-          {details[0].material.map((text, idx) => {
-            return <span key={idx}>{text}</span>;
-          })}
-        </dd>
-        <hr />
-        <dt className="recycling_title">
-          <button>
-            <h2>지속가능성 & 환경</h2>
-            <i className="ic-chevron" />
-          </button>
-        </dt>
-        <dd className="recycling_desc">{details[0].recycling}</dd>
-        <hr />
-        <dt className="package_title">
-          <button>
-            <h2>포장</h2>
-            <i className="ic-chevron" />
-          </button>
-        </dt>
-        <dd className="package_desc">
-          {details[0].package.map((text, idx) => {
-            return <span key={idx}>{text}</span>;
-          })}
-        </dd>
-        <hr />
-        <dt className="title">
-          <button>
-            <h2>조립 및 문서</h2>
-            <i className="ic-chevron" />
-          </button>
-        </dt>
-        <dd className="desc">정보없음</dd>
-        <hr />
-        <dt className="title">
-          <button>
-            <h2>고시정보</h2>
-            <i className="ic-chevron" />
-          </button>
-        </dt>
-        <dd className="desc">정보없음</dd>
-      </dl>
+        </div>
+        <ul className="accordion">
+          <Detail title={TITLE[0]}>
+            {details[0].material.map((text, idx) => {
+              return <span key={idx}>{text}</span>;
+            })}
+          </Detail>
+          <Detail title={TITLE[1]}>{details[0].recycling}</Detail>
+          <Detail title={TITLE[2]}>
+            {details[0].package.map((text, idx) => {
+              return <span key={idx}>{text}</span>;
+            })}
+          </Detail>
+          <Detail title={TITLE[3]}>정보없음</Detail>
+          <Detail title={TITLE[4]}>정보없음</Detail>
+        </ul>
+      </div>
     );
   }
 }
 
 export default DetailsModal;
+const TITLE = [
+  '소재 관리',
+  '지속가능성 & 환경',
+  '포장',
+  '조립 및 문서',
+  '고시정보',
+];
