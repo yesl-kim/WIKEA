42fe4fe6c720fb37f117031a0ed8157381c4135a
SUYEON <gsy4568@gmail.com>
2021-05-18 16:40:54 +0900

    Add : btn 상수데이터화, README삭제

--- src/pages/Main/Lists/ListBtn/FilterModal/FilterModal.js
+++ src/pages/Main/Lists/ListBtn/FilterModal/FilterModal.js
@@ -5,46 +5,6 @@ class FilterModal extends React.Component {
   constructor() {
     super();
     this.state = {
-      btnTitle: [
-        {
-          name: 'sortbtn',
-          title: '정렬',
-          list: [
-            '베스트 매치',
-            '낮은 가격 순',
-            '높은 가격 순',
-            '최신',
-            '고객 평가',
-            '이름',
-            '가장 인기있는',
-            '폭',
-            '신장',
-          ],
-        },
-        {
-          name: 'pricebtn',
-          title: '가격',
-          list: [
-            '₩ 0 - 9,999',
-            '₩ 10,000 - 19,999',
-            '₩ 20,000 - 29,999',
-            '₩ 30,000 - 39,999',
-            '₩ 40,000+',
-          ],
-        },
-        {
-          name: 'colorbtn',
-          title: '색깔',
-          list: ['red', 'blue', 'green'],
-        },
-      ],
-      checkboxTitle: [
-        { title: '온라인 특가' },
-
-        { title: '신상품' },
-
-        { title: '특가' },
-      ],
       sortbtn: false,
       pricebtn: false,
       colorbtn: false,
@@ -52,7 +12,7 @@ class FilterModal extends React.Component {
   }
 
   handleBtn = e => {
-    const btnsValue = e.currentTarget.value;
+    const btnsValue = e.currentTarget.name;
 
     this.setState({
       [btnsValue]: !this.state[btnsValue],
@@ -60,17 +20,15 @@ class FilterModal extends React.Component {
   };
 
   render() {
-    const { btnTitle, checkboxTitle } = this.state;
-
     return (
       <ul className="filter_modal">
-        {btnTitle.map((btns, index) => (
+        {BTNS_TITLE.btnTitle.map((btns, index) => (
           <li className="filter_title" key={index}>
             <button
               type="button"
               className="filter_btn"
               onClick={this.handleBtn}
-              value={`${btns.name}`}
+              name={btns.name}
             >
               <div className="filter_name">
                 <span>{btns.title}</span>
@@ -79,25 +37,23 @@ class FilterModal extends React.Component {
                 <i className="ic-caret" />
               </div>
             </button>
-            <ul
-              className={`filter_box ${
-                !this.state[btns.name] && 'visually-hidden'
-              }`}
-            >
-              <li className="filter_lists">
-                {btns.list.map((btnList, index) => (
-                  <label className="radio_btn" key={index}>
-                    <span>{btnList}</span>
-                    <div className="radio">
-                      <input type="radio" id="radio" name="radioName" />
-                    </div>
-                  </label>
-                ))}
-              </li>
-            </ul>
+            {this.state[btns.name] && (
+              <ul className="filter_box">
+                <li className="filter_lists">
+                  {btns.list.map((btnList, index) => (
+                    <label className="radio_btn" key={index}>
+                      <span>{btnList}</span>
+                      <div className="radio">
+                        <input type="radio" id="radio" name="radioName" />
+                      </div>
+                    </label>
+                  ))}
+                </li>
+              </ul>
+            )}
           </li>
         ))}
-        {checkboxTitle.map((checkboxBtn, index) => (
+        {BTNS_TITLE.checkboxTitle.map((checkboxBtn, index) => (
           <li className="filter_title" key={index}>
             <label type="button" className="filter_btn">
               <div className="filter_name">
@@ -115,3 +71,46 @@ class FilterModal extends React.Component {
 }
 
 export default FilterModal;
+
+const BTNS_TITLE = {
+  btnTitle: [
+    {
+      name: 'sortbtn',
+      title: '정렬',
+      list: [
+        '베스트 매치',
+        '낮은 가격 순',
+        '높은 가격 순',
+        '최신',
+        '고객 평가',
+        '이름',
+        '가장 인기있는',
+        '폭',
+        '신장',
+      ],
+    },
+    {
+      name: 'pricebtn',
+      title: '가격',
+      list: [
+        '₩ 0 - 9,999',
+        '₩ 10,000 - 19,999',
+        '₩ 20,000 - 29,999',
+        '₩ 30,000 - 39,999',
+        '₩ 40,000+',
+      ],
+    },
+    {
+      name: 'colorbtn',
+      title: '색깔',
+      list: ['red', 'blue', 'green'],
+    },
+  ],
+  checkboxTitle: [
+    { title: '온라인 특가' },
+
+    { title: '신상품' },
+
+    { title: '특가' },
+  ],
+};
