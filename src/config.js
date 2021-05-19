25d5fe5664f3288fa8d7b86108d1b3a21bcad9b3
SUYEON <gsy4568@gmail.com>
2021-05-19 12:15:30 +0900

    브랜치 합치기 중간단계

--- src/config.js
+++ src/config.js
@@ -1,14 +1,14 @@
 const IP = '172.30.1.47:8000';
 export const API = {
   SIGN_IN: `http://${IP}/users/signin`,
-<<<<<<< HEAD
-<<<<<<< HEAD
+  // <<<<<<< HEAD
+  // <<<<<<< HEAD
   DETAILS: `http://${IP}/product/detail/`,
-=======
+  // =======
   CATEGORY: '/data/Categories/Categories.json',
->>>>>>> origin/feature/mainNavRouting
-=======
+  // >>>>>>> origin/feature/mainNavRouting
+  // =======
   NEW_PRODUCT: `http://${IP}/product/newList`,
   NEW_PRODUCT_TEST: `/data/NewProducts/NewProducts.json`,
->>>>>>> origin/feature/main
+  // >>>>>>> origin/feature/main
 };
