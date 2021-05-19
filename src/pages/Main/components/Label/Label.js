455840bfd103e5372ebe2e1fb8bccf19ac22071b
Kim Yeseul <96linda@hanmail.net>
2021-05-19 18:56:46 +0900

    Add : label 위치값 추가

--- src/pages/Main/components/Label/Label.js
+++ src/pages/Main/components/Label/Label.js
@@ -6,13 +6,13 @@ class Label extends Component {
   render() {
     const { number, product } = this.props;
     return (
-      <div className={`label_container label${number}_${product.id}`}>
+      <div className={`label_container new${number}_${product.id}`}>
         <button type="button" className="label_btn" />
         <Link to="/" className="label">
           <p className="product_info">
             {product.is_new && <span className="new">New</span>}
             <span className="name">{product.korean_name}</span>
-            <span className="category">{product.category}</span>
+            <span className="category">{product.sub_category}</span>
             <div className="price">
               <span className="won">￦</span>
               <span>{Number(product.price).toLocaleString('ko-KR')}</span>
