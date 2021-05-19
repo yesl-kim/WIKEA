7b90a0e13c5bd3b15e15b78c512d4b79d65cf87d
Kim Yeseul <96linda@hanmail.net>
2021-05-19 14:58:42 +0900

    Add : api 테스트

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
-const IP = '172.30.1.47:8000';
+const IP = '172.30.1.2:8000';
 export const API = {
   SIGN_IN: `http://${IP}/users/signin`,
-  NEW_PRODUCT: `http://${IP}/product/newList`,
+  NEW_PRODUCT: `http://${IP}/product/newlist`,
   NEW_PRODUCT_TEST: `/data/NewProducts/NewProducts.json`,
 };
