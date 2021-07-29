2a97a8edec1cc2389765c22da6e563ddab6054af
Kim Yeseul <yesl00k624@gmail.com>
2021-07-29 19:55:39 +0900

    Add: SubCategory - 하위카테고리 컴포넌트 분리

--- src/components/SubCategory/index.js
+++ src/components/SubCategory/index.js
@@ -1,47 +1,47 @@
-import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
-import './AsideNav.scss';
+import React from 'react';
+import { useHistory, Link } from 'react-router-dom';
+import './index.scss';
 
-class AsideNav extends Component {
-  goToSubCat = subCat => {
-    this.props.history.push(`/lists/${subCat}`);
-    this.props.handleSideModalOn();
+export const SubCategory = ({
+  isOpened,
+  title,
+  list,
+  handleSideModalOn,
+  children,
+}) => {
+  const history = useHistory();
+  const goToList = subCategoryName => {
+    history.push(`/lists/${subCategoryName}`);
+    handleSideModalOn();
   };
 
-  render() {
-    const { isClicked, on, type, title, list, handleClick, children } =
-      this.props;
-    return (
-      <nav
-        className={on ? `aside_nav ${type}_menu on` : `aside_nav ${type}_menu`}
-      >
-        <h2 className="aside_nav_title">{title}</h2>
-        <ul aria-label={`${title} 하위 메뉴`} className="menu_container">
-          <li>{type === 'main' ? '지속가능한 제품' : '전체 보기'}</li>
-          {!!list.length &&
-            list.map((item, idx) => (
-              <li
-                key={item.id}
-                role="button"
-                onClick={
-                  type === 'main'
-                    ? () => handleClick(idx, item.korean_name)
-                    : () => this.goToSubCat(item.english_name)
-                }
-              >
-                {item.korean_name}
-                {type === 'main' && (
-                  <i
-                    className={isClicked[idx] ? 'ic-chevron on' : 'ic-chevron'}
-                  />
-                )}
-              </li>
-            ))}
-        </ul>
-        {children}
-      </nav>
-    );
-  }
-}
-
-export default withRouter(AsideNav);
+  return (
+    <nav className={title ? 'sub_category on' : 'sub_category'}>
+      <h2 className="nav_title">{title}</h2>
+      <ul aria-label={`${title} 하위 메뉴`} className="menu_container">
+        <li>전체 보기</li>
+        {!!list &&
+          list.map(item => (
+            <li
+              key={item.id}
+              role="button"
+              onClick={() => goToList(item.english_name)}
+            >
+              {item.korean_name}
+            </li>
+          ))}
+      </ul>
+      <div className="menu_promotion">
+        <Link to="/">
+          <span className="promotion_image">
+            <img
+              alt="지속가능한 LED 전구 구매 가이드"
+              src="https://images.unsplash.com/photo-1513506003901-1e6a229e2d15?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80"
+            />
+          </span>
+          <span>지속가능한 LED 전구 구매 가이드</span>
+        </Link>
+      </div>
+    </nav>
+  );
+};
