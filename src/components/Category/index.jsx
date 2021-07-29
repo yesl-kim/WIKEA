e2db052a700e74401108d770bad38205f37d27bb
Kim Yeseul <yesl00k624@gmail.com>
2021-07-29 19:56:22 +0900

    Modify: MainNavModal 리팩토링

--- src/components/Category/index.jsx
+++ src/components/Category/index.jsx
@@ -1,30 +1,38 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './index.scss';
 
-export const Category = ({ list, handleClick, clickedItem, children }) => {
+export const Category = ({ list, handleClick, activeItem, children }) => {
   return (
-    <nav className={clickedItem ? `category on` : `category`}>
-      <h2 className="aside_nav_title">모든 제품</h2>
+    <nav className={activeItem ? `category on` : `category`}>
+      <h2 className="nav_title">모든 제품</h2>
       <ul aria-label="모든 제품 카테고리" className="menu_container">
         <li>지속가능한 제품</li>
-        {list.map((item, idx) => (
+        {list.map(item => (
           <li
             key={item.id}
             role="button"
-            onClick={() => handleClick(idx, item.korean_name)}
+            onClick={() => handleClick(item.korean_name)}
           >
             {item.korean_name}
             <i
               className={
-                clickedItem === item.korean_name
-                  ? 'ic-chevron on'
-                  : 'ic-chevron'
+                activeItem === item.korean_name ? 'ic-chevron on' : 'ic-chevron'
               }
             />
           </li>
         ))}
       </ul>
-      {children}
+      <div className="menu_promotion">
+        <span>최근 본 제품</span>
+        <ul className="promotion_list">
+          <li>
+            <Link to="/lists/">
+              <img alt="조명" src="/images/products/lamps/10-r png.png" />
+            </Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 };
